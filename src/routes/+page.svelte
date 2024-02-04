1f288bca31288bc9d7c36c474d90dc8c60e8d06b
Viktor Daniel Varga <vvd007@gmail.com>
2024-02-04 11:27:29 +0100

    Basic todos functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,63 @@
+<script lang="ts">
+  // STATE
+  let todoText = "";
+  let editedTodoId: null | number;
+  let todos = [
+    { id: 0, text: "an item", finished: false },
+    { id: 1, text: "Second item", finished: false },
+    { id: 2, text: "A third item too", finished: false },
+  ];
+
+  // HELPER FUNCS
+  const resetTodoInput = () => {
+    editedTodoId = null;
+    todoText = "";
+  };
+
+  // EVENTS
+  const clickOnSaveTodo = () => {
+    if (typeof editedTodoId === "number") {
+      todos.find((t) => t.id === editedTodoId)!.text = todoText;
+      todos = todos;
+    } else {
+      todos = [
+        ...todos,
+        { id: todos.length + 1, text: todoText, finished: false },
+      ];
+    }
+
+    resetTodoInput();
+  };
+  const clickOnTodo = (id: number) => {
+    todos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.finished = !todo.finished;
+      }
+
+      return todo;
+    });
+  };
+  const clickOnDeleteTodo = (id: number) => {
+    if (
+      confirm(
+        `Do you really want to delete "${
+          todos.find((t) => t.id === id)?.text
+        }"?`
+      )
+    ) {
+      todos = todos.filter((t) => t.id !== id);
+
+      if (editedTodoId === id) {
+        resetTodoInput();
+      }
+    }
+  };
+  const clickOnEditTodo = (id: number) => {
+    editedTodoId = id;
+    todoText = todos.find((t) => t.id === id)!.text;
+  };
+</script>
+
 <div class="container">
   <h1>Todos with Svelte</h1>
 
@@ -7,28 +67,38 @@
       class="form-control"
       placeholder="Todo"
       aria-label="Todo"
+      bind:value={todoText}
     />
-    <button class="btn btn-primary" type="button">
+    <button class="btn btn-primary" type="button" on:click={clickOnSaveTodo}>
       <i class="fa-solid fa-floppy-disk"></i>
     </button>
   </div>
 
   <ul class="list-group">
-    <li class="list-group-item">
-      <input type="checkbox" /> An item
-    </li>
-    <li class="list-group-item">
-      <input type="checkbox" /> A second item
-    </li>
-    <li class="list-group-item">
-      <input type="checkbox" /> A third item
-    </li>
-    <li class="list-group-item">
-      <input type="checkbox" /> A fourth item
-    </li>
-    <li class="list-group-item">
-      <input type="checkbox" /> And a fifth one
-    </li>
+    {#each todos as todo}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+      <li class="list-group-item" on:click={() => clickOnTodo(todo.id)}>
+        <span>
+          <input type="checkbox" bind:checked={todo.finished} />
+          {todo.text}
+        </span>
+
+        <span class="todo-operations">
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          <i
+            class="fa-solid fa-trash text-danger"
+            on:click|stopPropagation={() => clickOnDeleteTodo(todo.id)}
+          ></i>
+
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          <i
+            class="fa-solid fa-pencil text-warning"
+            on:click|stopPropagation={() => clickOnEditTodo(todo.id)}
+          ></i>
+        </span>
+      </li>
+    {/each}
   </ul>
 </div>
 
@@ -45,5 +115,16 @@
   .list-group-item {
     display: flex;
     gap: 10px;
+    justify-content: space-between;
+  }
+
+  .todo-operations {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .todo-operations i {
+    cursor: pointer;
   }
 </style>
