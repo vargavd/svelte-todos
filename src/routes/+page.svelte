50f6eb27861b0265351d40948f1b0bb9e97d625a
Viktor Daniel Varga <vvd007@gmail.com>
2024-02-04 10:57:42 +0100

    Basic layout (bootstrap)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <h1>Todos with Svelte</h1>
+
+  <div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Todo"
+      aria-label="Todo"
+    />
+    <button class="btn btn-primary" type="button">
+      <i class="fa-solid fa-floppy-disk"></i>
+    </button>
+  </div>
+
+  <ul class="list-group">
+    <li class="list-group-item">
+      <input type="checkbox" /> An item
+    </li>
+    <li class="list-group-item">
+      <input type="checkbox" /> A second item
+    </li>
+    <li class="list-group-item">
+      <input type="checkbox" /> A third item
+    </li>
+    <li class="list-group-item">
+      <input type="checkbox" /> A fourth item
+    </li>
+    <li class="list-group-item">
+      <input type="checkbox" /> And a fifth one
+    </li>
+  </ul>
+</div>
+
+<style>
+  h1 {
+    font-size: 3rem;
+    font-family: "Roboto";
+  }
+
+  .container {
+    max-width: 500px;
+  }
+
+  .list-group-item {
+    display: flex;
+    gap: 10px;
+  }
+</style>
